33a7f153f06bacb30df9e602da476b869404aeda
bartvdbraak <bartvdbraak@gmail.com>
2019-04-24 02:33:46 +0200

    csv files, html controls, js custom script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready( function () {
     $('#masterTable').DataTable();
 } );
+
+function filterEntries() {
+
+}
+
+function addPercentages() {
+
+}
+
+function applyChanges() {
+    filterEntries();
+    addPercentages();
+    alert('Done.')
+}
