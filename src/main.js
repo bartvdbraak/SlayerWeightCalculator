cdc46021dcca78e61d0e167a7812e6c7f58bbec4
bartvdbraak <bartvdbraak@gmail.com>
2020-01-31 01:24:51 +0100

    yarn add, import, use buefy

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 new Vue({
   render: h => h(App),
