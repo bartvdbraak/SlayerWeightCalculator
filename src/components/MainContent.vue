cd021fe9d256b45e6c49494dc636755e32334cb0
bartvdbraak <bartvdbraak@gmail.com>
2020-01-31 01:59:58 +0100

    remove helloworld, add nav/content components, install bootstrap (and dependencies)

---
+++ src/components/MainContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{ headerText }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainContent',
+  props: {
+    headerText: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item">
+                <img src="https://vignette.wikia.nocookie.net/2007scape/images/b/be/Mr._Mordaut_chathead.png">
+            </a>
+        </div>
+        <div class="navbar-menu">
+            <div class="navbar-start">
+            </div>
+            <div class="navbar-end">
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
