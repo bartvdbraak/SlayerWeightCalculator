e47b338d7fbb2b8d021d1327c86cd22f2db4f15c
bartvdbraak <bartvdbraak@gmail.com>
2020-01-31 01:59:58 +0100

    remove helloworld, add nav/content components, install bootstrap (and dependencies)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar></navbar>
+    <main-content headerText="test message"></main-content>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainContent from './components/MainContent.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MainContent,
+    Navbar,
   }
 }
 </script>
@@ -21,8 +23,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
-Vue.use(Buefy)
 
 new Vue({
   render: h => h(App),
